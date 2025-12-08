c51640df06f59faa51ed4fa71fdde43f2ebff6e1
GOD20aa <this.is.mymagic20@gmail.com>
2025-12-08 01:05:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -504,33 +504,11 @@ async def main():
         token = os.getenv("DISCORD_TOKEN")
         await bot.start(token)
 
-
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
 
 
 
-import asyncio
-
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot is running!"
-
-# REPLIT: keep_alive webserver (ha kell)
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
-keep_alive()
 
 
-asyncio.run(main())
